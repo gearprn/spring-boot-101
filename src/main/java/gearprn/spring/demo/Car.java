b8ed82e8f4b6cb06801b29ae5dd4d908f8e626a1
gearprn <gearprn@gmail.com>
2020-09-08 15:06:59 +0700

    Add: Car model

---
+++ src/main/java/gearprn/spring/demo/Car.java
@@ -0,0 +1,59 @@
+package gearprn.spring.demo;
+
+public class Car {
+    private int id;
+    private String color;
+    private String type;
+    private int displacement;
+    private boolean sunroof;
+    private int speed;
+    static int totalCar;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getDisplacement() {
+        return displacement;
+    }
+
+    public void setDisplacement(int displacement) {
+        this.displacement = displacement;
+    }
+
+    public boolean isSunroof() {
+        return sunroof;
+    }
+
+    public void setSunroof(boolean sunroof) {
+        this.sunroof = sunroof;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+}
